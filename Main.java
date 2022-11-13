d9aa04868561d959ba684a34e982b84b41c0b8bf
Donna Gabriel <89215411+donnagabriel@users.noreply.github.com>
2022-11-13 15:29:53 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,6 @@
 package application;
 
-import java.util.ArrayList;
-
 import javafx.application.Application;
-import javafx.event.ActionEvent;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.stage.Stage;
@@ -14,7 +11,6 @@ import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TitledPane;
 import javafx.scene.control.ToggleGroup;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
